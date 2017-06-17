9acdfa9a13686ec103aee7dfbe18c0319a18e61b
stef <radusi.stefan@gmail.com>
2017-06-17 15:15:38 +0300

    prime

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Index = function Index() {
+  return _react2.default.createElement("div", null, _react2.default.createElement("p", null, "Hello Next.js"));
+};
+
+exports.default = Index;
